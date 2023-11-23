97433dc53ffc4a8b890b3982f30eed69c5237d79
Muxammadbobur <bobur0172@gmail.com>
2023-11-24 03:30:46 +0500

    storage fix

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -27,7 +27,7 @@ export const useCartStore = create(
     }),
     {
       name: "food-storage", // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
     }
   )
 );
